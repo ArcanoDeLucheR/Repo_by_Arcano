053c508796dd6f970496c64b12382dc9aa55b0f1
ArcanoDeLucheR <Mr.Kzs2@mail.ru>
2019-02-01 21:42:25 +0300

    Добавлены функции

--- sandbox/src/main/java/ru/stqa/arcano/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/arcano/sanbox/MyFirstProgram.java
@@ -4,7 +4,26 @@ public class MyFirstProgram
 {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("Arcano");
+
+    double l = 5;
+    System.out.println("Площать квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Площать прмоугольника со сторонами " + l + " = " + area(a,b));
+
   }
 
+  public static void  hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+  public static double area (double len){
+    return len* len;
+  }
+  public static double area (double a, double b){
+    return a * b;
+  }
 }
