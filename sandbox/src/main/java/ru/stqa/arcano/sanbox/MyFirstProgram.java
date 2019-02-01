7bb9f210eab74445a290c70cfb3a0c0a277f4313
ArcanoDeLucheR <Mr.Kzs2@mail.ru>
2019-02-01 20:15:14 +0300

    Создан пакет дял моей первой прогрммы

--- sandbox/src/main/java/ru/stqa/arcano/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/arcano/sanbox/MyFirstProgram.java
@@ -1,6 +1,10 @@
-public class MyFirstProgram {
+package ru.stqa.arcano.sanbox;
+
+public class MyFirstProgram
+{
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
   }
+
 }
