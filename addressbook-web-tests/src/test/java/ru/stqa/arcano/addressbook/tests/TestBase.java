5eea62daf432105b42a4a075e2e8ab0c381da21d
ArcanoDeLucheR <Mr.Kzs2@mail.ru>
2019-02-16 11:15:54 +0300

    if for driver

--- addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/TestBase.java
@@ -2,6 +2,7 @@ package ru.stqa.arcano.addressbook.tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.arcano.addressbook.appmanager.ApplicationManager;
@@ -9,7 +10,7 @@ import ru.stqa.arcano.addressbook.model.ContactData;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
