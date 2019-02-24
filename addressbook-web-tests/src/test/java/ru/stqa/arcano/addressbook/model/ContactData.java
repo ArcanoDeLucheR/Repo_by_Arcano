04fb19e1938692aaca6d4cb972a69fcfa0ce408a
ArcanoDeLucheR <Mr.Kzs2@mail.ru>
2019-02-24 13:00:21 +0300

    4.9

--- addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/model/ContactData.java
@@ -40,8 +40,26 @@ public class ContactData {
     return id;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
+
   public ContactData(String firstname, String lastname, String nickname, String title, String company, String address, String mobile , String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.nickname = nickname;
@@ -54,26 +72,6 @@ public class ContactData {
 
   public String getFirstname() {
     return firstname;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    ContactData that = (ContactData) o;
-
-    if (id != that.id) return false;
-    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-    return result;
   }
 
   public String getLastname() {
