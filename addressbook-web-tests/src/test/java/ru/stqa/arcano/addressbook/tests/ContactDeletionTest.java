783d0c0bef48119fa2f5994e81c5adb7053666ea
ArcanoDeLucheR <Mr.Kzs2@mail.ru>
2019-02-24 13:04:09 +0300

    Задание №9: Реализовать проверки путём сравнения списков

--- addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/ContactDeletionTest.java
@@ -3,20 +3,20 @@ package ru.stqa.arcano.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.arcano.addressbook.model.ContactData;
-import ru.stqa.arcano.addressbook.model.GroupData;
 
+
+import java.util.Comparator;
 import java.util.List;
 
-public class ContactDeletionTest extends TestBase{
+public class ContactDeletionTest extends TestBase {
 
   @Test
   public void testContactDeletion() throws Exception {
     app.getContactHelper().homePage();
-    if (! app.getContactHelper().isThereAContact())
-    {
-              app.getNavigationHelper().addNewPage();
-              app.getContactHelper().addContact(new ContactData("Альберт", "Эйнштейн", "Гений", "Физик", null, "Германия, Уильм", "8800444333", null), true);
-              app.getContactHelper().homePage();
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getNavigationHelper().addNewPage();
+      app.getContactHelper().addContact(new ContactData("Альберт", "Эйнштейн", "Гений", "Физик", null, "Германия, Уильм", "8800444333", null), true);
+      app.getContactHelper().homePage();
     }
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size() - 1);
@@ -27,8 +27,12 @@ public class ContactDeletionTest extends TestBase{
 
 
     before.remove(before.size() - 1);
-    Assert.assertEquals(before, after);
 
+    Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+
+    Assert.assertEquals(before, after);
 
 
   }
