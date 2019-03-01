981ce5c5708c3d75b617378466f27d0ca87387bb
ArcanoDeLucheR <Mr.Kzs2@mail.ru>
2019-03-01 19:52:45 +0300

    5.1

--- addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/TestBase.java
@@ -1,23 +1,21 @@
 package ru.stqa.arcano.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 import ru.stqa.arcano.addressbook.appmanager.ApplicationManager;
-import ru.stqa.arcano.addressbook.model.ContactData;
+
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterSuite
   public void tearDown() throws Exception {
     app.stop();
   }
