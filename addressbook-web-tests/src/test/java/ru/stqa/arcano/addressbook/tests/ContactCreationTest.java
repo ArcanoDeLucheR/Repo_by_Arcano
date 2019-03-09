88b8d3698e8d20bb3f20a1c03a48f89b6e0ecc57
ArcanoDeLucheR <Mr.Kzs2@mail.ru>
2019-03-09 19:15:27 +0300

    Задание №13: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/arcano/addressbook/tests/ContactCreationTest.java
@@ -1,34 +1,59 @@
 package ru.stqa.arcano.addressbook.tests;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.arcano.addressbook.model.ContactData;
 import ru.stqa.arcano.addressbook.model.Contacts;
 
 
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class ContactCreationTest extends TestBase {
 
-  @Test()
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.json"));
+    String json = "";
+    String line = reader.readLine();
+    while (line !=null){
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); //List<ContactData>.class
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) throws Exception {
     app.contact().homePage();
     Contacts before = (Contacts) app.contact().all();
     app.goTo().addNewPage();
-    File photo = new File("src/test/resources/1.jpg");
-    ContactData contact = new ContactData()
-            .withFirstname("Альберт")
-            .withLastname("Эйнштейн")
-            .withNickname("Гений")
-            .withTitle("Физик")
-            .withAddress("Германия, Уильм")
-            .withCompany("E=mc^")
-            .withHomePhone("8800444333")
-            .withMobilePhone("+7 (800) 333-333")
-            .withEmail_1("albert_einstein@mail.ru")
-            .withEmail_3("relativity@пmail.ru")
-            .withPhoto(photo);
+  //  File photo = new File("src/test/resources/1.jpg");
+  //  ContactData contact = new ContactData()
+  //          .withFirstname("Альберт")
+  //          .withLastname("Эйнштейн")
+  //          .withNickname("Гений")
+  //          .withTitle("Физик")
+   //         .withAddress("Германия, Уильм")
+  //          .withCompany("E=mc^")
+ //           .withHomePhone("8800444333")
+  //          .withMobilePhone("+7 (800) 333-333")
+   //         .withEmail_1("albert_einstein@mail.ru")
+   //         .withEmail_3("relativity@пmail.ru")
+  //          .withPhoto(photo);
 
     app.contact().addContact(contact,true);
     app.contact().homePage();
